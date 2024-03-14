72a3b7e03579be06814c15db4d6b61750c6ec459
richarddg10 <91717302+richarddg10@users.noreply.github.com>
2024-03-14 10:27:43 -0500

    Update InstructionsPage.jsx

--- src/pages/InstructionsPage/InstructionsPage.jsx
+++ src/pages/InstructionsPage/InstructionsPage.jsx
@@ -13,13 +13,13 @@ export function InstructionsPage() {
                 <div>
                     <ol className="instructions-list">
                         <li>Mínimo 2 jugadores - máximo 4 jugadores.</li>
-                        <li>Cada equipo empieza con $ 100 dólares.</li>
-                        <li>El ganador es el equipo que termine con más dinero.</li>
-                        <li>El juego termina cuando completan todas las casillas o cuando uno de los equipos quede sin dólares.</li>
-                        <li>Cada vez que un equipo pierda en la adivinanza uno de los integrantes debe tomar medio shot.</li>
-                        <li>Los equipos solo pueden tirar la canica una vez por ronda.</li>
-                        <li>Los dólares que pierden los equipos se guardan en el casino.</li>
-                        <li>Los equipos tendrán 1min máximo para cumplir los retos y adivinanzas.</li>
+                        <li>Cada jugador empieza con $ 100 dólares.</li>
+                        <li>El ganador es el jugador que termine con más dinero.</li>
+                        <li>El juego termina cuando uno delos jugadores llegue a $500 dólares o alguno se quede sin dinero.</li>
+                        <li>La casilla azul significa que pasa el turno sin penitencia</li>
+                        <li>Los jugadores solo pueden tirar la canica una vez por ronda.</li>
+                        <li>Los dólares que pierden los jugadores se guardan en el casino.</li>
+                        <li>Los jugadores tendrán 1min máximo para cumplir los retos y adivinanzas.</li>
                     </ol>
                 </div>
                 <Buttons className='instructions-button main-button' onClick={() => navigate('/teams-page')} placeholder='Siguiente' />
